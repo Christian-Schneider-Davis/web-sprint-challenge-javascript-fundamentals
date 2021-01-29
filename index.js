fefcdf6048da39d5d92ce1098e6c2c00aa31a5f0
Christian Schneider-Davis <75326583+Christian-Schneider-Davis@users.noreply.github.com>
2021-01-29 12:34:05 -0600

    save 5

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const zooAnimals = [
 
   function lowerCaseNames(){
     const lowerCase = zooAnimals.map(animal_name.toLowerCase());
-    return lowerCase
+    console.log(lowerCase);
   }
   
   
@@ -83,9 +83,11 @@ const zooAnimals = [
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+  function lowPopulationAnimals(newNew){
+    let newArray = zooAnimals.filter();
+      return zooAnimals.population < 5
+    }
+  
   
 
   /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
@@ -94,9 +96,11 @@ const zooAnimals = [
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+  function USApop(newReduce){
+    const newSet = zooAnimals.reduce((total, state) => {
+      return total += state.population; }, 0);
+      }
+  
   
   
   // 🦁🦁🦁 Callbacks 🦁🦁🦁  
